e76c4d4f80ad4591c16ee2cd76222ba7ccc747e0
Aleksey <aljasones@yandex.ru>
2019-09-26 12:40:29 +0300

    fixes #2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,54 +5,66 @@ public class Main {
         Tariff myOnline = new Tariff();
         myOnline.id = 1;
         myOnline.name = "Мой Онлайн";
-        myOnline.costOnMonthInRubles = 290;
-        myOnline.trafficVolumeInGb = 15;
         myOnline.unlimMessengers = true;
         myOnline.volumeOfMinutes = 400;
         myOnline.umnlimMinutesOnTele2 = true;
+        myOnline.tariffPaymentOnRubles = 290;
+        myOnline.payment = Payment.onMonth;
+        myOnline.trafficVolume = 15;
+        myOnline.traffic = Traffic.gigabyte;
+
 
         Tariff myOnline2 = new Tariff();
         myOnline2.id = 2;
         myOnline2.name = "Мой Онлайн+";
-        myOnline2.costOnMonthInRubles = 350;
-        myOnline2.trafficVolumeInGb = 20;
         myOnline2.unlimMessengers = true;
         myOnline2.unlimInternetAbroad = true;
         myOnline2.volumeOfMinutes = 600;
         myOnline2.umnlimMinutesOnTele2 = true;
+        myOnline2.tariffPaymentOnRubles = 350;
+        myOnline2.payment = Payment.onMonth;
+        myOnline2.trafficVolume = 20;
+        myOnline2.traffic = Traffic.gigabyte;
 
         Tariff mySpeak = new Tariff();
         mySpeak.id = 3;
         mySpeak.name = "Мой Разговор";
-        mySpeak.costOnMonthInRubles = 190;
-        mySpeak.trafficVolumeInGb = 3;
         mySpeak.volumeOfMinutes = 250;
         mySpeak.umnlimMinutesOnTele2 = true;
+        mySpeak.tariffPaymentOnRubles = 190;
+        mySpeak.payment = Payment.onMonth;
+        mySpeak.trafficVolume = 3;
+        mySpeak.traffic = Traffic.gigabyte;
 
         Tariff myTele2 = new Tariff();
         myTele2.id = 4;
         myTele2.name = "Мой Tele2";
-        myTele2.costOnDayInRubles = 7;
-        myTele2.trafficVolumeInGb = 6;
         myTele2.umnlimMinutesOnTele2 = true;
+        myTele2.tariffPaymentOnRubles = 7;
+        myTele2.payment = Payment.onDay;
+        myTele2.trafficVolume = 6;
+        myTele2.traffic = Traffic.gigabyte;
 
         Tariff unlim = new Tariff();
         unlim.id = 5;
         unlim.name = "Безлимитный";
-        unlim.costOnMonthInRubles = 400;
         unlim.unlimTraffic = true;
         unlim.volumeOfMinutes = 500;
         unlim.umnlimMinutesOnTele2 = true;
         unlim.volumeOfSms = 50;
+        unlim.tariffPaymentOnRubles = 400;
+        unlim.payment = Payment.onMonth;
 
         Tariff premium = new Tariff();
         premium.id = 6;
         premium.name = "Премиум";
-        premium.costOnMonthInRubles = 1_100;
-        premium.trafficVolumeInGb = 40;
         premium.volumeOfMinutes = 2_000;
         premium.umnlimMinutesOnTele2 = true;
         premium.volumeOfSms = 500;
+        premium.tariffPaymentOnRubles = 1_100;
+        premium.payment = Payment.onMonth;
+        premium.trafficVolume = 40;
+        premium.traffic = Traffic.gigabyte;
 
         Tariff classic = new Tariff();
         classic.id = 7;
@@ -67,10 +79,12 @@ public class Main {
         Tariff internetForThings = new Tariff();
         internetForThings.id = 9;
         internetForThings.name = "Интернет для вещей";
-        internetForThings.costOnMonthInRubles = 100;
-        internetForThings.trafficVolumeInMb = 300;
         internetForThings.volumeOfMinutes = 30;
         internetForThings.volumeOfSms = 100;
+        internenForDevices.tariffPaymentOnRubles = 100;
+        internenForDevices.payment = Payment.onMonth;
+        internenForDevices.trafficVolume = 300;
+        internenForDevices.traffic = Traffic.megabyte;
 
     }
 }
--- src/main/java/Tariff.java
+++ src/main/java/Tariff.java
@@ -2,15 +2,25 @@ public class Tariff {
 
     int id;
     String name;
-    int costOnMonthInRubles;
-    int costOnDayInRubles;
     boolean withoutSubscriptionFee;
-    int trafficVolumeInGb;
-    int trafficVolumeInMb;
     boolean unlimTraffic;
     boolean unlimMessengers;
     boolean unlimInternetAbroad;
     int volumeOfMinutes;
     boolean umnlimMinutesOnTele2;
     int volumeOfSms;
+    int tariffPaymentOnRubles;
+    public Payment payment;
+    int trafficVolume;
+    public Traffic traffic;
 }
+
+    enum Payment {
+        onMonth,
+        onDay
+    }
+
+    enum Traffic {
+        gigabyte,
+        megabyte
+    }
